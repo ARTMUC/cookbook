4cb0156d8b60dffb089883b7773f476778118d56
ARTMUC <89223118+ARTMUC@users.noreply.github.com>
2021-09-22 12:41:18 +0200

    Frontend revision
    added basic navbar component
    added routing
    added screens

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import "./screens/UserScreen";
+
+//components
+
+import UserScreen from "./screens/UserScreen";
+import HomeScreen from "./screens/HomeScreen"
+import Navbar from "./components/Navbar";
+
+import { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      {/* navbar component needs to go here */}
+<Navbar/>
+     
+
+      <main>
+            <Switch>
+        <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/user-screen" component={UserScreen} />
+        </Switch>
+      </main>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,36 @@
+import "./Navbar.css";
+import mainLogo from "./OIP.jpg"
+import { SiCodechef } from 'react-icons/si'
+
+
+import { useState } from "react";
+
+function Navbar() {
+  return (
+    <div className="App">
+      <div>
+        <header className="header">
+          <ul className="container">
+< SiCodechef className="container__item--logo"/>
+
+         
+
+            <li className="container__item--center">
+            <a className="button button1" href="/">home screen</a>
+    
+      <a className="button button1" href="/user-screen">user screen</a>
+             
+            </li>
+
+            <li className="container__item--right">
+              <a className="button button1" href="https://www.google.com">google it</a>
+            </li>
+          </ul>
+        </header>
+        <main>...</main>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
